1e78ba55679c4fc78af2d5e58de6afe106fc6e46
deadcxde <71565965+deadcxde@users.noreply.github.com>
2023-12-02 02:44:39 +0300

    Update ChestFarm.lua

--- ChestFarm.lua
+++ ChestFarm.lua
@@ -1,7 +1,7 @@
 _G.ChestFarmEnabled = 1 -- 1 - Включено, 0 - Выключено | Включить/Выключить фарм сундуков
 _G.BypassAntiCheat = 1 -- 1 - Включено, 0 - Выключено | Включить/Выключить обход античита
 _G.WebhookURL = 'https://discord.com/api/webhooks/1174720347526922292/8RAWz_Wm6jeScxTgYr0GhmJPRwLYBLL8cWfgh3g4UXz3qHF4CH8yU-ArleUEhqk4oaT8' -- Webhook Ссылка, куда будут приходить оповещения с статистикой персонажа (Ник, кол-во бели) / Подробно можно у меня узнать по этой хуйне
-_G.WebhookTimer = 60 -- Каждые X секунд
+_G.WebhookTimer = 99 -- Каждые X секунд
 _G.BypassTP = true
 _G.HopWhenNoChest = true
 _G.StopWhenBeli = 2000000 -- Остановить фарм когда больше чем X бели
